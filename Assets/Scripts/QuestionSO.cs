3fe65cbc28f9b72e4b3f0b64ea477a7f962da736
l7aur <gradlaur@yahoo.com>
2024-08-04 12:33:18 +0300

    added functionality to gui

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,27 +7,36 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "Question")]
 public class QuestionSO : ScriptableObject
 {
+    private const int n = 4;
+    private List<string> scrambledAnswers;
+
     [TextArea(1, 6)] //number of lines of text displayed in unity
     [SerializeField] private string question = "Enter question text here.";
-
-    [SerializeField] private List<string> answers = new(4);
-    private readonly List<string> scrambledAnswers;
+    [SerializeField] private List<string> answers = new(n);
     [SerializeField] private string correctAnswer;
 
     public void ShuffleAnswers()
     {
-        System.Random random = new System.Random();
-        List<string> scrambledAnswers = answers.OrderBy(x => random.Next()).ToList();
-        answers = scrambledAnswers;
+        System.Random random = new();
+        scrambledAnswers = answers.OrderBy(x => random.Next()).ToList();
     }
 
     public string GetQuestion() { return question; }
     public string GetCorrectAnswer() { return correctAnswer; }
+
+    public int GetCorrectAnswerIndex()
+    {
+        for(int i = 0; i < n; i++)
+            if(scrambledAnswers.ElementAt(i).Equals(correctAnswer))
+                return i;
+        return -1;
+    }
+
     public string GetAnswerIndex(int answerIndex)
     {
         try
         {
-            return answers[answerIndex];
+            return scrambledAnswers.ElementAt(answerIndex);
         }
         catch(IndexOutOfRangeException e)
         {
--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Linq;
+using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
+    [SerializeField] QuestionSO question;
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] GameObject[] answerButtons;
+    [SerializeField] Sprite wrongAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        question.ShuffleAnswers();
+        questionText.text = question.GetQuestion();
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons.ElementAt(i).GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswerIndex(i);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnAnswerSelected(int answerID)
     {
-        
+        Image correctButtonImage = answerButtons[question.GetCorrectAnswerIndex()].GetComponent<Image>();
+        correctButtonImage.sprite = correctAnswerSprite;
+
+        if (question.GetAnswerIndex(answerID).Equals(question.GetCorrectAnswer()))
+            questionText.text = "Correct!";
+        else
+        {
+            questionText.text = "Wrong!";
+            Image chosenButton = answerButtons[answerID].GetComponent<Image>();
+            chosenButton.sprite = wrongAnswerSprite;
+        }
     }
 }
