d4c21c673784beda8eb2629acdaab2c551f7ce01
l7aur <gradlaur@yahoo.com>
2024-08-04 14:24:39 +0300

    timer

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -5,14 +5,62 @@ using UnityEngine.UI;
 
 public class Quiz : MonoBehaviour
 {
+    [Header("Questions")]
     [SerializeField] QuestionSO question;
     [SerializeField] TextMeshProUGUI questionText;
+    
+    [Header("Answers")]
     [SerializeField] GameObject[] answerButtons;
+    private bool hasAnsweredEarly = false;
+
+    [Header("Button Sprites")]
     [SerializeField] Sprite wrongAnswerSprite;
     [SerializeField] Sprite correctAnswerSprite;
+    [SerializeField] Sprite defaultAnswerSprite;
+
+    [Header("Timer")]
+    [SerializeField] Image timerImage;
+    Timer timer;
 
     // Start is called before the first frame update
     void Start()
+    {
+        timer = FindObjectOfType<Timer>();
+        GetNextQuestion();
+    }
+
+    void Update()
+    {
+        timerImage.fillAmount = timer.GetFillFraction();
+        if (timer.GetLoadNextQ())
+        {
+            GetNextQuestion();
+            timer.SetLoadNextQ(false);
+        }
+        else if(!hasAnsweredEarly && !timer.GetIsAnsweringToQ())
+        {
+            DisplayAnswer(-1);
+            SetButtonInteractability(false);
+        }
+    }
+
+    private void GetNextQuestion()
+    {
+        SetButtonInteractability(true);
+        ResetButtonSprites();
+        DisplayQuestionAndAnswers();
+    }
+
+    private void ResetButtonSprites()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Image image = answerButtons.ElementAt(i).GetComponent<Image>();
+            image.sprite = defaultAnswerSprite; 
+        }
+    }
+
+    private void DisplayQuestionAndAnswers()
     {
         question.ShuffleAnswers();
         questionText.text = question.GetQuestion();
@@ -25,9 +73,17 @@ public class Quiz : MonoBehaviour
 
     public void OnAnswerSelected(int answerID)
     {
+        DisplayAnswer(answerID);
+        SetButtonInteractability(false);
+        timer.CancelTimer();
+    }
+
+    private void DisplayAnswer(int answerID)
+    {
         Image correctButtonImage = answerButtons[question.GetCorrectAnswerIndex()].GetComponent<Image>();
         correctButtonImage.sprite = correctAnswerSprite;
-
+        if (answerID < 0) 
+            return;
         if (question.GetAnswerIndex(answerID).Equals(question.GetCorrectAnswer()))
             questionText.text = "Correct!";
         else
@@ -37,4 +93,14 @@ public class Quiz : MonoBehaviour
             chosenButton.sprite = wrongAnswerSprite;
         }
     }
+
+    void SetButtonInteractability(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons.ElementAt(i).GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    float timerValue;
+
+    private bool loadNextQ = true;
+    private bool isAnsweringToQ = true;
+    private float fillFraction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerValue = timeToCompleteQuestion;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimer();
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+        if (isAnsweringToQ)
+        {
+            if (timerValue > 0)
+                fillFraction = timerValue / timeToCompleteQuestion;
+            else
+            {
+                isAnsweringToQ = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if (timerValue > 0)
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            else
+            {
+                isAnsweringToQ = true;
+                loadNextQ = true;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
+    }
+
+    public void CancelTimer()
+    {
+        timerValue = 0f;
+    }
+
+    public float GetFillFraction() { return fillFraction; }
+    public float GetTimerValue() { return timerValue; }
+    public bool GetIsAnsweringToQ() { return isAnsweringToQ; }
+    public bool GetLoadNextQ() {  return loadNextQ; }
+    public void SetLoadNextQ(bool state) { loadNextQ = state; }
+}
